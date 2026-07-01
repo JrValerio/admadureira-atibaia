64ea92c0e63d7433db0e855457b2b57579c1489b
JrValerio <amarovsjr81@gmail.com>
2026-07-01 10:38:03 -0300

    perf(images): limita variantes responsivas a 2048px

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
     "/*": ["./public/programacao/semanas/**/*"],
   },
   images: {
+    deviceSizes: [360, 414, 640, 750, 828, 1080, 1200, 1600, 1920, 2048],
     remotePatterns: [
       {
         protocol: "https",
