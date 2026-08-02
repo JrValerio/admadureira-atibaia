f239f067b30900da8658c76081861255bdce50e1
JrValerio <amarovsjr81@gmail.com>
2026-08-02 00:11:22 -0300

    Atualiza artes da semana de 3 de agosto

--- src/data/banner-semanal-manifest.ts
+++ src/data/banner-semanal-manifest.ts
@@ -281,6 +281,18 @@ export const BANNERS_SEMANAIS_DISPONIVEIS: Record<string, readonly string[]> = {
     "vigilia.png"
   ],
   "2026-08-03": [
+    "circulo-de-oracao.png",
+    "consagracao-mulheres.png",
+    "culto-de-domingo.png",
+    "culto-de-ensino.png",
+    "culto-de-libertacao.png",
+    "culto-de-quinta.png",
+    "ebd.png",
+    "ensaio-irmas.png",
+    "ensaio-jovens.png",
+    "jejum-e-oração.png",
+    "oracao-matinal-domingo.png",
+    "oracao-matinal.png",
     "santa-ceia.png"
   ],
   "2026-08-31": [
