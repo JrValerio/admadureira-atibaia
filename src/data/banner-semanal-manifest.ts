af60230d363876407f30cacd69b1aa56b70bd3e6
JrValerio <amarovsjr81@gmail.com>
2026-08-24 19:42:38 -0300

    Atualiza artes da semana de 24 a 30 de agosto

--- src/data/banner-semanal-manifest.ts
+++ src/data/banner-semanal-manifest.ts
@@ -326,6 +326,21 @@ export const BANNERS_SEMANAIS_DISPONIVEIS: Record<string, readonly string[]> = {
     "oracao-matinal.png",
     "vigilia.png"
   ],
+  "2026-08-24": [
+    "circulo-de-oracao.png",
+    "consagracao-mulheres.png",
+    "culto-de-domingo.png",
+    "culto-de-ensino.png",
+    "culto-de-libertacao.png",
+    "culto-de-quinta.png",
+    "ebd.png",
+    "ensaio-irmas.png",
+    "ensaio-jovens.png",
+    "jejum-e-oração.png",
+    "oracao-matinal-domingo.png",
+    "oracao-matinal.png",
+    "vigilia.png"
+  ],
   "2026-08-31": [
     "aniversario-pastor-zacarias.png"
   ],
